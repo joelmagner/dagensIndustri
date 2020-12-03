0e7832e7f6b302fa9aef69347080341bd2a72e79
joelmagner <joelemagner@gmail.com>
2020-12-03 22:21:58 +0100

    cleanup

--- server/src/components/rss.component.ts
+++ server/src/components/rss.component.ts
@@ -3,10 +3,11 @@ import Parser from "rss-parser";
 export class RssComponent {
   private parser: Parser;
 
+  constructor() {
+    this.parser = new Parser();
+  }
+
   public async fetchRSS(url: string): Promise<Parser.Output> {
-    if (this.parser == null || undefined) {
-      this.parser = new Parser();
-    }
     return await this.parser.parseURL(url);
   }
 }
