fe18ac6350ceecd73b305049a91dc62e4627f4d3
joelmagner <joelemagner@gmail.com>
2021-02-06 12:25:17 +0100

    Added possibility to view more than one rss-feed at a time

--- web/src/components/PostFeed.tsx
+++ web/src/components/PostFeed.tsx
@@ -1,24 +1,46 @@
 import { Box, Stack } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { usePostsQuery } from "../generated/graphql";
 import { Post } from "./Post";
 interface PostFeedProps {}
 
 export const PostFeed: React.FC<PostFeedProps> = ({}) => {
-  const [variables, setVariables] = useState({
+  const [diVar, __] = useState({
     limit: 10,
     url: "https://wwww.di.se/rss",
     order: true,
   });
-  const [{ data, error, fetching }] = usePostsQuery({
-    variables,
+
+  const [dnVar, _] = useState({
+    limit: 10,
+    url: "https://wwww.dn.se/rss",
+    order: true,
   });
 
-  if (!fetching && !data) {
+  const [postData, setPostData] = useState([]);
+
+  const [
+    { data: diData, error: diError, fetching: diFetching },
+  ] = usePostsQuery({
+    variables: diVar,
+  });
+
+  const [
+    { data: dnData, error: dnError, fetching: dnFetching },
+  ] = usePostsQuery({
+    variables: dnVar,
+  });
+
+  useEffect(() => {
+    setPostData(diData?.posts);
+    dnData?.posts.map((p) => postData?.push(p));
+  }, [diData, dnData]);
+
+  if (!diFetching && !diData) {
     return (
       <div>
         <div>Your Query failed.</div>
-        <div>{error.message}</div>
+        <div>{diError.message}</div>
       </div>
     );
   }
@@ -26,11 +48,12 @@ export const PostFeed: React.FC<PostFeedProps> = ({}) => {
   return (
     <>
       <Box mt={8} mx="auto" w="50vw">
-        {!data && fetching ? (
+        {console.log("dwadwa", postData)}
+        {!postData && (diFetching || dnFetching) ? (
           <div>Loading...</div>
         ) : (
           <Stack spacing={8}>
-            {data!.posts.map((p) =>
+            {postData?.map((p) =>
               !p ? null : <Post key={p.guid + p.isoDate} post={p} />
             )}
           </Stack>
